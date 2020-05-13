6ff5e172be463b1ed28983c0658b01ed8ee6ab6c
yalvarado <yago.alvarado@bglgroup.co.uk>
2020-05-13 11:07:41 +0100

    first commit

---
+++ Controllers/DeveloperInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperInfoService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeveloperInfoController : ControllerBase
+    {
+        [HttpGet]
+        public DeveloperInfo Get()
+        {
+            return new DeveloperInfo
+            {
+                DeveloperName = "Perico Palotes",
+                ProgrammingLanguage = ".Net/C#",
+                Timestamp = DateTime.Now.ToString("yyyyMMddhhmmss")
+            };
+        }
+    }
+}
---
+++ DeveloperInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperInfoService
+{
+    public class DeveloperInfo
+    {
+        public string DeveloperName { get; set; }
+        public string ProgrammingLanguage { get; set; }
+        public string Timestamp { get; set; }
+    }
+}
